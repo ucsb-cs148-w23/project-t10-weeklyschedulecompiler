b27460b300185230648dae9df5552988463fcc7a
Cyril Wang <cyrilwang@umail.ucsb.edu>
2023-01-20 01:15:55 -0800

    cw - initial commit of basic hello world web app

---
+++ helloworld/frontend/src/main/components/Nav/Footer.js
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+
+export default function Footer() {
+  return (
+    <footer className="bg-light pt-3 pt-md-4 pb-4 pb-md-5">
+      <Container>
+        <p>
+        </p>
+      </Container>
+    </footer>
+  );
+}
