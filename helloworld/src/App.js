c818cd58c6aa1d1da6c30e38923cadcee11836fa
Luke Li <lukewli@ucsb.edu>
2023-01-24 16:00:20 -0800

    Editing Hello World

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -1,30 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-import Container from 'react-bootstrap/Container'
+import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Stack from 'react-bootstrap/Stack';
 import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
-  return (<>
-    <Navbar>
+  return (
+    <>
+      <Navbar>
+        <Container>
+          <Navbar.Brand>Home Page</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <a>Profile</a>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <Container>
-        <Navbar.Brand >Home Page</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            <a>Profile</a>
-          </Navbar.Text>
-        </Navbar.Collapse>
+        <div
+          style={{ height: '100vh' }}
+          className="d-flex justify-content-center align-items-center"
+        >
+          <div
+            style={{ backgroundColor: 'Orange', height: '35%', width: '35%' }}
+            className="d-flex justify-content-center align-items-center"
+          >
+            <h1> Hello World </h1>
+          </div>
+        </div>
       </Container>
-    </Navbar>
-    <Container>
-      <div style={{height: '100vh'}} className='d-flex justify-content-center align-items-center'>
-      <div style={{backgroundColor: 'Orange', height: '35%', width: '35%' }} className='d-flex justify-content-center align-items-center'>
-        <h1> Hello World </h1>
-      </div>
-      </div>
-    </Container>
     </>
   );
 }
