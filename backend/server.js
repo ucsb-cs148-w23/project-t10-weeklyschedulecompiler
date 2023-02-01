87a7835d01d1675b09bf7fb6600d05760ae43b4b
Luke Li <lukewli@ucsb.edu>
2023-01-31 17:52:58 -0800

    LLDC - OAuth/scope permission works

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,53 @@
-// npm install dotenv
-require('dotenv').config()
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+require('./auth');
 
-// npm install express
-const express = require('express')
+const app = express();
 
-const app = express()
+function isLoggedIn(req, res, next) {
+  req.user ? next() : res.sendStatus(401);
+}
 
-app.use(express.json())
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+app.get('/', (req, res) => {
+  res.send('<a href="/auth/google">Authenticate with Google</a>');
+});
 
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT)
-})
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: [
+      'email',
+      'profile',
+      'https://www.googleapis.com/auth/calendar.events.readonly	',
+    ],
+  })
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', {
+    successRedirect: '/protected',
+    failureRedirect: '/auth/google/failure',
+  })
+);
+
+app.get('/protected', isLoggedIn, (req, res) => {
+  res.send(`Hello ${req.user.displayName}`);
+});
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.send('Goodbye!');
+});
+
+app.get('/auth/google/failure', (req, res) => {
+  res.send('Failed to authenticate..');
+});
+
+app.listen(8000, () => console.log('listening on port: 8000'));
