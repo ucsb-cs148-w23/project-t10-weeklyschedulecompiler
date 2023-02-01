3150e44c3e42ef9d443e9a433ea17dbca4b67514
Luke Li <lukewli@ucsb.edu>
2023-01-31 20:10:30 -0800

    LLDC - Connected frontend login to backend OAuth

--- backend/server.js
+++ backend/server.js
@@ -1,53 +1,57 @@
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
-require('./auth');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const authRoutes = require('./routes/auth-routes');
+const keys = require('./config/keys');
+
+const port = 8000;
 
 const app = express();
 
-function isLoggedIn(req, res, next) {
-  req.user ? next() : res.sendStatus(401);
-}
-
 app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+
+app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  })
+);
+
+app.use('/auth', authRoutes);
+
+const authCheck = (req, res, next) => {
+  if (!req.user) {
+    res.status(401).json({
+      authenticated: false,
+      message: 'user has not been authenticated',
+    });
+  } else {
+    next();
+  }
+};
 
 app.get('/', (req, res) => {
   res.send('<a href="/auth/google">Authenticate with Google</a>');
 });
 
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: [
-      'email',
-      'profile',
-      'https://www.googleapis.com/auth/calendar.events.readonly	',
-    ],
-  })
-);
-
-app.get(
-  '/auth/google/callback',
-  passport.authenticate('google', {
-    successRedirect: '/protected',
-    failureRedirect: '/auth/google/failure',
-  })
-);
-
-app.get('/protected', isLoggedIn, (req, res) => {
-  res.send(`Hello ${req.user.displayName}`);
+app.get('/check', authCheck, (req, res) => {
+  res.status(200).json({
+    authenticated: true,
+    message: 'user successfully authenticated',
+    user: req.user,
+    cookies: req.cookies,
+  });
 });
 
-app.get('/logout', (req, res) => {
-  req.logout();
-  req.session.destroy();
-  res.send('Goodbye!');
+app.get('/', (req, res) => {
+  res.send('<a href="/auth/google">Authenticate with Google</a>');
 });
 
-app.get('/auth/google/failure', (req, res) => {
-  res.send('Failed to authenticate..');
-});
-
-app.listen(8000, () => console.log('listening on port: 8000'));
+app.listen(port, () => console.log('listening on port:', port));
