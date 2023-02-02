4c622a677edc751676463f48abbefa58ab3ef1ea
tianyusun7 <tianyusun@ucsb.edu>
2023-02-02 14:56:30 -0800

    start createGroups
    ts-group controller and routerv1

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,72 @@
-require('dotenv').config()
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express() //initialize express app
+const authRoutes = require('./routes/auth-routes');
+const groupRoutes = require('./routes/groups');
+const keys = require('./config/keys');
+const config = require('./config');
 
-app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+const app = express();
+
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  })
+);
+
+app.use('/auth', authRoutes);
+app.use('/api/group', groupRoutes);
+
+const authCheck = (req, res, next) => {
+  if (!req.user) {
+    res.status(401).json({
+      authenticated: false,
+      message: 'user has not been authenticated',
+    });
+  } else {
+    next();
+  }
+};
 
 app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome'})
-})
+  res.send('<a href="/auth/google">Authenticate with Google</a>');
+});
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        //listen for requests
-        app.listen(process.env.PORT, () => {
-            console.log('connecting to db & listening on port', process.env.PORT)
+app.get('/check', authCheck, (req, res) => {
+  res.status(200).json({
+    authenticated: true,
+    message: 'user successfully authenticated',
+    user: req.user,
+    cookies: req.cookies,
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('<a href="/auth/google">Authenticate with Google</a>');
+});
+
+mongoose.set("strictQuery", true);
+
+mongoose.connect(config.mongoURI)
+    .then(() => { 
+        app.listen(config.port, () => {
+            console.log('connected to db & listening on port', config.port)
         })
     })
-    .catch((err) => {
-        console.log(err)
+    .catch((error) => {
+        console.log(error)
     })
+
 
