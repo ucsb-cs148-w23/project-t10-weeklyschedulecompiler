da38f766931190943efae1dc331f8013e45480f9
Maya Ma <yema@cs.ucsb.edu>
2023-02-03 14:03:15 -0800

    mm - Create Calender Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,17 @@ import { useState } from 'react';
 import AppNavbar from './components/Nav/AppNavbar';
 import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
+import CreateGroupPage from './pages/CreateGroupPage';
+import Cal from './components/calender/Cal';
+import WeekCal from './components/calender/WeekCal';
 function App() {
   const [user, setUser] = useState(true);
   return (
     <div className="App">
       <AppNavbar />
       <Home />
+      <Cal />
+      <WeekCal />
       {/* <Router>
       <AppNavbar />
         <Routes>
