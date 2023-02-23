7b9a18cd9715be1f6ebf00a6c026f469179914a1
Cyril Wang <cyrilwang@umail.ucsb.edu>
2023-02-23 12:03:21 -0800

    cw - added submit button

--- frontend/src/components/forms/CreateEventForm.js
+++ frontend/src/components/forms/CreateEventForm.js
@@ -4,21 +4,25 @@ import { Button } from 'react-bootstrap';
 export default function CreateEventForm() {
   return (
     <form>
-      <label className="me-1" style={{paddingLeft: '15%'}}>Event Name:   
+      <div>
+      <label className="me-1" style={{paddingLeft: '17%'}}>Event Name:   
       <input className="me-1" type="text"/>
       </label>
 
       <p></p>
 
-      <label className="me-1"  style={{paddingLeft: '15%'}}>Event Start Time:   
+      <label className="me-1"  style={{paddingLeft: '17%'}}>Event Start Time:   
       <input className="me-1" type="text"/>
       </label>
 
       <p></p>
 
-      <label className="me-1" style={{paddingLeft: '15%'}}>Event End Time:   
+      <label className="me-1" style={{paddingLeft: '17%'}}>Event End Time:   
       <input className="me-1"  type="text"/>
       </label>
+      </div>
+
+      <Button className="d-flex justify-content-center align-items-center mx-auto" style={{marginTop: '10%'}} type="submit">Submit</Button>
 
     </form>
   );
