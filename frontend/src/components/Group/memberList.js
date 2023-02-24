aa5853fcd6ea421b774cb7af99665745cca096d6
Luke Li <lukewli@ucsb.edu>
2023-02-24 14:35:10 -0800

    ll-rebasing

--- frontend/src/components/Group/memberList.js
+++ frontend/src/components/Group/memberList.js
@@ -16,7 +16,32 @@ export default function MemberList(props) {
             <Col
               className="d-flex justify-content-end"
               style={{ witdh: '100px' }}
-            ></Col>
+            >
+              <p
+                style={{
+                  cursor: 'pointer',
+                  position: 'absolute',
+                  right: '50px',
+                }}
+                onClick={() => {
+                  props.updateGroupMemberEvents(props.groupId, member[0]);
+                  setTimeout(() => {
+                    window.location.reload(false);
+                  }, 500);
+                }}
+              >
+                Refresh
+              </p>
+              {props.edit && (
+                <CloseButton
+                  onClick={() => {
+                    props.handleShow();
+                    props.setDelete(member[2]);
+                    props.setDelUser(member[1]);
+                  }}
+                ></CloseButton>
+              )}
+            </Col>
           </Row>
         </ListGroup.Item>
       ))}
