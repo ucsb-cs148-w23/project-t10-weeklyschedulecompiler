01b33fdff6b72febef0e60ef00574d1902502fdd
wesleykam <wesley_kam@ucsb.edu>
2023-03-02 18:49:51 -0800

    wk - create onClick actions for accept and decline buttons

--- frontend/src/components/Invite/InviteCard.js
+++ frontend/src/components/Invite/InviteCard.js
@@ -1,11 +1,28 @@
 import React from "react";
-import { Card, Col } from 'react-bootstrap';
+import { Card, Row, Col, Button } from 'react-bootstrap';
 
 function inviteCard( { groupName }) {
+
+    function acceptInvite() {
+        console.log('accept invite')
+    }
+    
+    function declineInvite() {
+        console.log('decline invite')
+    }
+
     return (
         <Card className="m-2">
             <Card.Body style={{ width: '100%', cursor: 'pointer' }}>
-                <Card.Title>{groupName}</Card.Title>
+                <Row>
+                    <Col style={{display:'flex', alignItems:'center'}}>
+                        <Card.Title>{groupName}</Card.Title> 
+                    </Col>
+                    <Col style={{display:'flex', justifyContent:'end'}}>
+                        <Button className='m-1' variant='success' onClick={() => {acceptInvite()}}>&#x2713;</Button>
+                        <Button className='m-1' variant='danger' onClick={() => {declineInvite()}}>&#x2715;</Button>
+                    </Col>
+                </Row>
             </Card.Body>
         </Card>
     )
